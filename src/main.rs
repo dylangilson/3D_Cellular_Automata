c35375dc6020dd754588ddb66dd64da0bc8d602f
dylangilson <dylan.gilson@outlook.com>
2023-01-20 00:07:01 -0800

    added cell_renderer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+/*
+ * Dylan Gilson
+ * dylan.gilson@outlook.com
+ * January 19, 2023
+ */
+
+// TODO: may have to add cells subfolder from github to out cells folder but i believe its for the options GUI
+
 fn main() {
     println!("Hello, world!");
 }
